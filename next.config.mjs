083210d7c9fe03ad742fa6a3a722d94b60c8b544
edsystemlocal <stretechsolutions@gmail.com>
2025-02-07 00:32:13 +0530

    three dot module

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true, // Useful for debugging
-    swcMinify: false, // Enable faster builds
+    swcMinify: true, // Enable faster builds
   };
   
   export default nextConfig;
